aae3b27366d7b34370105355aea9dc3ded543019
ILIHANDROOOO <110981204+ILIHANDRO@users.noreply.github.com>
2022-08-14 01:43:08 +0600

    Add files via upload

---
+++ Ilihandro don diego.py
@@ -0,0 +1,27 @@
+class Company:
+    def __init__(self, company_name, company_bank):
+        self.company_name = company_name
+        self.company_bank = company_bank
+
+class Person(Company):
+    def __init__(self, company_name, company_bank, first_name, last_name, salary):
+        super().__init__(company_name, company_bank)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+def get_salary(self):
+    if self.company_bank < self.salary:
+        print("У компании недостаточно средств")
+    else:
+        self.company_bank -= self.salary
+        print(f'Остаток капитала компании {self.company_bank}')
+
+def get_info(self):
+    print(f"Информация о сотруднике: {self.first_name}{self.last_name} зарплата: {self.salary}\n")
+
+bank = Person('Credits', 90000, 'Jon', 'Hedit', 40000)
+
+get_info(bank)
+get_salary(bank)
+
