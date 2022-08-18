68ba9559180bce53b51578c83b6e9dee82394158
ILIHANDROOOO <110981204+ILIHANDRO@users.noreply.github.com>
2022-08-18 18:25:41 +0600

    Add files via upload

---
+++ venv/test.py
@@ -0,0 +1,7 @@
+from hw.file_executor.create import create_file
+from hw.file_executor.write import write_to_file
+from hw.file_executor.delete import delete_file
+
+create_file('ilihandro', '.txt')
+write_to_file("ilihandro.txt", "67898765")
+delete_file("class.txt")
